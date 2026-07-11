71fd3aa5f7a21bb54b4dbf12a5a2ec0600492c8d
Hasaniii2451 <hasanhomayon666@gmail.com>
2026-07-11 12:36:31 +0330

    Create start.py

---
+++ handlers/handlers/start.py
@@ -0,0 +1,13 @@
+from aiogram import Router
+from aiogram.types import Message
+from aiogram.filters import Command
+
+router = Router()
+
+
+@router.message(Command("start"))
+async def start_handler(message: Message):
+    await message.answer(
+        "سلام 👋\n"
+        "ربات بررسی عضویت آماده است."
+    )
